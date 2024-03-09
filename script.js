294d5a19465791e0d6555919b4b68ecc938cadd8
manshouri <manshouri611@gmail.com>
2024-03-08 19:05:10 -0800

    add index and script.js files

---
+++ script.js
@@ -0,0 +1,13 @@
+const arr1 = [1,2,3];
+const arr2 = [4,1,2];
+const arr3 = arr2.reverse(); //2 1 4
+const arr4 = arr1.reverse(); //3,2,1
+const result = [];
+
+for(let i=0; i<arr3.length;i++){
+   result.push(arr3[i] + arr4[i]);
+   console.log(arr5)
+}
+
+
+
